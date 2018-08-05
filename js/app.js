217a4bf3b1ade02aca31453b9d46d8891c00c96c
Hung-Yun Liao <hungyunliao@gmail.com>
2018-08-05 01:49:25 -0700

    use 'with' binding

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-// This is just like the normal functions in JS
-//function ViewModel() {
-var ViewModel = function() {
+/* ===== MODEL ===== */
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.level = ko.computed(function() {
         if (this.clickCount() < 10) {
@@ -18,10 +17,22 @@ var ViewModel = function() {
         {nickName: 'B', pre: 'Sr.'},
         {nickName: 'C', pre: 'Jr.'}
     ]);
+};
+
+/* ===== CONTROLLER =====*/
+// This is just like the normal functions in JS
+//function ViewModel() {
+var ViewModel = function() {
+    var self = this; // A pointer pointing to the ViewModel itself which is out of the function scopes.
     
+    this.currentCat = ko.observable(new Cat());
     
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+        // Approuch 1: the 'this' here is in the 'with' binding context which means that 'this' here == 'this.currentCat()', so here we cannot use this.currentCat().clickCount anymore.
+//        this.clickCount(this.clickCount() + 1);
+        
+        // Approuch 2: use 'self' the ViewModel     
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 };
 
