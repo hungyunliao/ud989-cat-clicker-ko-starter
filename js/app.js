c0c0e6a8a5451a12c40f8737b0289bb05b1f3af0
Hung-Yun Liao <hungyunliao@gmail.com>
2018-08-04 20:07:28 -0700

    Add cat level

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+// This is just like the normal functions in JS
+//function ViewModel() {
 var ViewModel = function() {
     this.clickCount = ko.observable(0);
+    this.level = ko.computed(function() {
+        if (this.clickCount() < 10) {
+            return 'Infant';
+        } else if (this.clickCount() < 100) {
+            return 'Child';
+        } else {
+            return 'Teen';
+        }
+    }, this);   // Need to pass 'this' into the function so that it can be used inside the function.
     this.name = ko.observable('Taddy');
     this.imgSrc = ko.observable('./img/1413379559_412a540d29_z.jpg');
     
